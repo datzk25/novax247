3368c25df481b59ca4637f86a0473e115742d5e9
Tiến Đạt <130561352+datzk25@users.noreply.github.com>
2026-08-03 05:08:33 +0700

    Add initial Next.js configuration file

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// File: next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [],
+    domains: [],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
